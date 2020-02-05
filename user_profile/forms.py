428fc6e48c9a82495f19e4c6c306e6f9d7ee8462
Anjan Rai <rai.unknown@gmail.com>
2020-02-05 23:16:27 +1100

    Created EducationDateField with validator to validate against present date, Education model and EducationForm form. Further clean method defined to validate start and completion date dependency

---
+++ user_profile/forms.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+
+from resume_builder.user_profile.models import Education
+
+
+class EducationForm(ModelForm):
+    class Meta:
+        model = Education
+        fields = ['institute', 'level', 'faculty', 'start_date', 'ongoing', 'completion_date']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        cleaned_completion_date = cleaned_data.get('completion_date', None)
+        cleaned_start_date = cleaned_data.get('start_date')
+        cleaned_ongoing = cleaned_data.get('ongoing')
+
+        if not cleaned_ongoing:
+            if not cleaned_completion_date:
+                raise ValidationError("Education completion date not provided.")
+
+            elif cleaned_completion_date:
+                if cleaned_completion_date < cleaned_start_date:
+                    raise ValidationError("Education completion date can not be older than start date.")
+
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,26 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+from django.forms import DateField, ValidationError
+
+
+def validate_education_date(value):
+    if value > datetime.today():
+        raise ValidationError("This date can not be greater than present date.")
+    pass
+
+
+class EducationDateField(DateField):
+    default_validators = [validate_education_date]
+
+
+class Education(models):
+    institute = models.CharField(blank=False, null=False)
+    level = models.CharField(blank=False, null=False)
+    faculty = models.CharField(blank=False, null=False)
+    start_date = EducationDateField(blank=False, null=False)
+    ongoing = models.BooleanField(default=False)
+    completion_date = EducationDateField(blank=True)
+
